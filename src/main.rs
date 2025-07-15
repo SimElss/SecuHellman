368152692fe8b97de1ee8a8e8ac81943e028788a
SimElss <79011788+SimElss@users.noreply.github.com>
2025-07-15 19:06:38 +0200

    Initializing X_End
    First version with x_end with some bug
    V2 of the hellman Table logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use sha2::{Digest, Sha256};
+use core::hash;
 use std::path::{Path, PathBuf};
 use std::env;
 use rand::prelude::*;
@@ -41,16 +42,33 @@ fn main() {
     // Initialize random number generator
     let mut rng = rand::rng();
     let x0 = rng.random_range(0..MAX_DOMAIN);
-
-    
+    let mut x0_list : Vec<u64> = Vec::new();
+    let mut x_end_list : Vec<u64> = Vec::new();
     println!("Number of chains: {}", args.nchains);
 
     for chain in 0..args.nchains {
         let x0_bis = (x0 + chain) % MAX_DOMAIN; 
+        x0_list.push(x0_bis);
+        for column in 0..args.ncolumns {
+            // Hash the value using SHA256
+            let mut hasher = Sha256::new();
+            hasher.update(x0_bis.to_le_bytes());
+            let hash_result = hasher.finalize();   
 
-        println!("Generating chain {} with starting point: {}", x0_bis, x0);
+            let mut bytes = [0u8; 8];
+            bytes.copy_from_slice(&hash_result[..8]);
+            let mut reduc = u64::from_le_bytes(bytes);
+
+            reduc = reduc.rotate_right(1);
+            let x_end = reduc & ((1u64 << 38) - 1);  
+
+        }
+        x_end_list.push(x_end);
         
     }
+    println!("Start = {}, End = {}", x0_list.last().unwrap() , x_end_list.last().unwrap());
+
+
     println!("Number of columns: {}", args.ncolumns);
     println!("Path to tables: {}", args.path.display());
     
